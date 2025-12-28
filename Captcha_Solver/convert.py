170dd7f35f7a9cd7a8a16a60dfb7c20d931208a3
doritoes <39832079+doritoes@users.noreply.github.com>
2025-12-28 18:33:34 -0500

    Update convert.py

--- Captcha_Solver/convert.py
+++ Captcha_Solver/convert.py
@@ -1,6 +1,7 @@
+import os
+import sys
 import torch
 import openvino as ov
-import os
 from train import SimpleCNN
 
 # 1. Load the "High Accuracy" weights
@@ -11,7 +12,7 @@ if os.path.exists("captcha_model.pth"):
     print("Loaded weights from captcha_model.pth")
 else:
     print("Error: captcha_model.pth not found! Run train.py first.")
-    exit()
+    sys.exit()
 
 # 2. Convert to OpenVINO IR Format
 print("Converting PyTorch model to OpenVINO IR... (This is fast)")
