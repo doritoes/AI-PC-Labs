31a6867271a8f2fc365766804d622a3002ba83d7
doritoes <39832079+doritoes@users.noreply.github.com>
2025-12-29 13:06:21 -0500

    Update config.py

--- Captcha_Solver/advanced/config.py
+++ Captcha_Solver/advanced/config.py
@@ -4,7 +4,7 @@ import string
 CHARS = string.digits + string.ascii_lowercase + string.ascii_uppercase
 CAPTCHA_LENGTH = 6
 WIDTH, HEIGHT = 200, 80  # Increased size for 6 characters
-DATASET_SIZE = 50000      # Base images
-TOTAL_SIZE = 100000      # With 1:1 augmentation (Rotate/Blur)
-BATCH_SIZE = 128
+DATASET_SIZE = 20000      # Base images, add 1:2 augmentation for total 60k
+TOTAL_SIZE = 60000      # With 1:2 augmentation (Rotate/Blur)
+BATCH_SIZE = 32 # reduced to work in reduced RAM
 DEVICE = "xpu"           # Intel iGPU/dGPU via IPEX
