91449588c81570796a61f88bf53691bece1b754e
doritoes <39832079+doritoes@users.noreply.github.com>
2026-01-05 23:11:21 -0500

    Update config.py

--- Captcha_Solver/advanced/config.py
+++ Captcha_Solver/advanced/config.py
@@ -1,7 +1,7 @@
 import string
 
 # --- Dataset Settings ---
-DATASET_SIZE = 20000 
+DATASET_SIZE = 12000 
 CAPTCHA_LENGTH = 6
 # Keep the full set here so the model architecture creates all 62 output nodes
 CHARS = string.digits + string.ascii_lowercase + string.ascii_uppercase
@@ -9,7 +9,7 @@ WIDTH = 200
 HEIGHT = 80
 
 # --- Training Hyperparameters ---
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 # Increased slightly to help the model "jump" during the Digits phase
 LEARNING_RATE = 0.002 
 # 50 Epochs total (5 Digits + 45 Full Alphanumeric)
